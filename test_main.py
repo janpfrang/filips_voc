74d16129b6e5b00b07d56fad4cdbec177247db31
janpfrang <59746919+janpfrang@users.noreply.github.com>
2025-11-13 15:13:29 +0100

    Add initial test cases in test_main.py
    Tests for CI built

---
+++ test_main.py
@@ -0,0 +1,10 @@
+# test_main.py
+def test_example():
+    """Einfacher Test, der immer durchläuft"""
+    assert 1 + 1 == 2
+    
+def test_import():
+    """Test, ob dein Hauptmodul importiert werden kann"""
+    # Falls du eine main.py hast:
+    # import main
+    assert True
